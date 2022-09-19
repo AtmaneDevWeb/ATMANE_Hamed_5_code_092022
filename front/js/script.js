502abf44dfbb418f4ed6b24f6a79066af5d8da37
AtmaneDevWebgit commit -m first <hamed.atmane@gmx.fr>
2022-09-19 19:33:35 +0200

    Implémentation du code (script.js) pour la page d'accueil terminé: récupération des produits dans le backend et affichage de ceux-ci sur la page d'accueil.

--- front/js/script.js
+++ front/js/script.js
@@ -1,19 +1,41 @@
 //Création de la requête qui récupère l'ensemble des produits sur l'api
-fetch("https://localhost:3000/api/products")
+fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
     .then((data) => addProducts(data))
     .then((err) => console.log(err))
 
 //Création des articles
-function addProducts(donnees) {
-    let id = donnees[0]._id
+function addProducts(sofaList) {
+for (let i=0; i < sofaList.length; i++) {
 
-//Création de la balise <a> + anchor
-    let anchor = document.createElement("a")
-        anchor.href = "./product.html?id=" + id
-        anchor.text = "lien vers l'id du produit"
-//Récupération de l'item + rattachement de l'anchor
-    let items = document.querySelector("#items")
-        items.appendChild(anchor)
-        console.log(items)
-}
+    //Création de la balise <a> + récupération des id produits
+        let a = document.createElement("a");
+            a.href = `product.html?id=${sofaList[i]._id}`;
+        
+
+    //Récupération de l'item + injection de la balise <a> 
+        let items = document.querySelector("#items");
+            items.appendChild(a);
+    
+    //Création et injection de la balise <a> dans <aticle>
+        let article = document.createElement("article");
+            a.appendChild(article);
+
+    //Création et injection de l'image
+        let kanapImg = document.createElement("img");
+            kanapImg.src = sofaList[i].imageUrl;
+            kanapImg.alt = sofaList[i].altTxt;
+            article.appendChild(kanapImg);
+
+    //Création et injection du h3
+        let kanapName = document.createElement("h3");
+            kanapName.classList.add("productName");
+            kanapName.innerHTML = sofaList[i].name;
+            article.appendChild(kanapName);
+
+    //Création et injection du paragraphe p
+        let kanapDescription = document.createElement("p");
+            kanapDescription.classList.add("productDescription");
+            kanapDescription.innerHTML = sofaList[i].description;
+            article.appendChild(kanapDescription);
+}}
