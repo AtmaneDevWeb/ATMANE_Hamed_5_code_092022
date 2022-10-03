24b93a46a7f0da6eb6c200bc64d2f694ef277ff2
AtmaneDevWebgit commit -m first <hamed.atmane@gmx.fr>
2022-10-03 11:07:26 +0200

    Modification des innerHTML en texContent, modification des captures d'erreurs dans le fetch, ajouts de produits dans le panier (id, qty, color), diverses modifiations.

--- front/js/product.js
+++ front/js/product.js
@@ -7,61 +7,64 @@ let Sname;
 
 // Création de la requête qui récupère les "id" des produits sur l'api
 fetch(`http://localhost:3000/api/products/${id}`)
-    .then((res) => res.json())
-    .then((res) => handleData(res))
-    .then((err) => console.log(err))
+  .then((res) => res.json())
+  .then((res) => handleData(res))
+  .catch((error) => {
+    console.log(error);
+  });
 
- // Affichage des caractéristiques du produit
- function handleData(sofa) {
-    // Affichage de l'image du produit
-    let imgItem = document.createElement("img");
-        document.querySelector(".item__img").appendChild(imgItem);
-        imgItem.src = sofa.imageUrl;
-        imgItem.alt = sofa.altTxt;
+// Affichage des caractéristiques du produit
+function handleData(sofa) {
+  // Affichage de l'image du produit
+  let imgItem = document.createElement("img");
+  document.querySelector(".item__img").appendChild(imgItem);
+  imgItem.src = sofa.imageUrl;
+  imgItem.alt = sofa.altTxt;
 
-    // Affichage du nom du produit
-    document.querySelector("#title").textContent = sofa.name;
+  // Affichage du nom du produit
+  document.querySelector("#title").textContent = sofa.name;
 
-    // Affichage du prix du produit
-    document.querySelector("#price").textContent = sofa.price;
+  // Affichage du prix du produit
+  document.querySelector("#price").textContent = sofa.price;
 
-    // Affichage de la description du produit
-    document.querySelector("#description").textContent = sofa.description;
+  // Affichage de la description du produit
+  document.querySelector("#description").textContent = sofa.description;
 
-    // Affichage des couleurs disponibles
-    for (let color of sofa.colors) {
-        let colorOfSofa = document.createElement("option");
-            document.querySelector("#colors").appendChild(colorOfSofa);
-            colorOfSofa.value = color;
-            colorOfSofa.textContent = color;
-        }
+  // Affichage des couleurs disponibles
+  for (let color of sofa.colors) {
+    let colorOfSofa = document.createElement("option");
+    document.querySelector("#colors").appendChild(colorOfSofa);
+    colorOfSofa.value = color;
+    colorOfSofa.textContent = color;
+  }
 
-        // Ajouter un article dans le panier
-        let adCartBtn = document.querySelector("#addToCart");
-        adCartBtn.addEventListener("click", () => {
-        const color = document.querySelector("#colors").value;
-        const quantity = document.querySelector("#quantity").value;
-        const sPrice = document.querySelector("#price").textContent;
-        const Sname = document.querySelector("#title").textContent;
-        
-        if (color == null || color == "" || quantity == null || quantity == 0) {
-            alert("Veuillez sélectionner une couleur ainsi qu'une quantité !");
-            return
-         }
-         // Création de l'objet qui sera stocké dans le localStorage qui va nous permettre de gérer le panier
-         let data = {
-            id: id,
-            color: color,
-            quantity: Number(quantity),
-            sPrice: Number(sPrice),
-            imgUrl: imgItem.src,
-            altText: imgItem.alt,
-            Sname: Sname
-        }
+  // Ajouter un article dans le panier
+  let adCartBtn = document.querySelector("#addToCart");
+  adCartBtn.addEventListener("click", () => {
+    const color = document.querySelector("#colors").value;
+    const quantity = document.querySelector("#quantity").value;
+    const sPrice = document.querySelector("#price").textContent;
+    const Sname = document.querySelector("#title").textContent;
 
-        // Stockage des données dans le localStorage et renvoi vers le panier
-        localStorage.setItem(id, JSON.stringify(data));
-        window.location.href = "cart.html";
+    if (color == null || color == "" || quantity == null || quantity == 0) {
+      alert("Veuillez sélectionner une couleur ainsi qu'une quantité !");
+      return;
     }
-    )
+    // Création de l'objet qui sera stocké dans le localStorage qui va nous permettre de gérer le panier
+    let data = {
+      id: id,
+      color: color,
+      quantity: Number(quantity),
+      // sPrice: Number(sPrice),
+      imgUrl: imgItem.src,
+      altText: imgItem.alt,
+      Sname: Sname,
+    };
+
+    // Stockage des données dans le localStorage et renvoi vers le panier
+    localStorage.setItem(id, JSON.stringify(data));
+    if (confirm("Produit ajouté, voulez vous aller à la page panier")) {
+      window.location.href = "cart.html";
+    }
+  });
 }
--- front/js/script.js
+++ front/js/script.js
@@ -1,41 +1,40 @@
 //Création de la requête qui récupère l'ensemble des produits sur l'api
 fetch("http://localhost:3000/api/products")
-    .then((res) => res.json())
-    .then((data) => addProducts(data))
-    .then((err) => console.log(err))
+  .then((res) => res.json())
+  .then((data) => addProducts(data))
+  .catch((err) => console.log(err));
 
 //Création des articles
 function addProducts(sofaList) {
-for (let i=0; i < sofaList.length; i++) {
-
+  for (let i = 0; i < sofaList.length; i++) {
     //Création de la balise <a> + récupération des id produits
-        let a = document.createElement("a");
-            a.href = `product.html?id=${sofaList[i]._id}`;
-        
+    let a = document.createElement("a");
+    a.href = `product.html?id=${sofaList[i]._id}`;
 
-    //Récupération de l'item + injection de la balise <a> 
-        let items = document.querySelector("#items");
-            items.appendChild(a);
-    
+    //Récupération de l'item + injection de la balise <a>
+    let items = document.querySelector("#items");
+    items.appendChild(a);
+
     //Création et injection de la balise <a> dans <aticle>
-        let article = document.createElement("article");
-            a.appendChild(article);
+    let article = document.createElement("article");
+    a.appendChild(article);
 
     //Création et injection de l'image
-        let kanapImg = document.createElement("img");
-            kanapImg.src = sofaList[i].imageUrl;
-            kanapImg.alt = sofaList[i].altTxt;
-            article.appendChild(kanapImg);
+    let kanapImg = document.createElement("img");
+    kanapImg.src = sofaList[i].imageUrl;
+    kanapImg.alt = sofaList[i].altTxt;
+    article.appendChild(kanapImg);
 
     //Création et injection du h3
-        let kanapName = document.createElement("h3");
-            kanapName.classList.add("productName");
-            kanapName.innerHTML = sofaList[i].name;
-            article.appendChild(kanapName);
+    let kanapName = document.createElement("h3");
+    kanapName.classList.add("productName");
+    kanapName.textContent = sofaList[i].name;
+    article.appendChild(kanapName);
 
     //Création et injection du paragraphe p
-        let kanapDescription = document.createElement("p");
-            kanapDescription.classList.add("productDescription");
-            kanapDescription.innerHTML = sofaList[i].description;
-            article.appendChild(kanapDescription);
-}}
+    let kanapDescription = document.createElement("p");
+    kanapDescription.classList.add("productDescription");
+    kanapDescription.textContent = sofaList[i].description;
+    article.appendChild(kanapDescription);
+  }
+}
